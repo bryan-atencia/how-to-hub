7133cb56c58c98571f9c48925a192c60e435e03e
bryan-atencia <bryan.atencia@thetiebar.com>
2020-06-23 20:30:05 -0500

    one more

--- dist/admin/functions/fetchHowTos.js
+++ dist/admin/functions/fetchHowTos.js
@@ -3,12 +3,14 @@ const { join } = require('path');
 const path = require("path")
 
 exports.handler = function(event, context, callback ) {
-  const filePath = path.join(__dirname, 'dist/admin/collections/How-tos');
+  const filePath = '/dist/admin/collections/How-tos';
   let folder = fs.readdirSync(filePath).map(x => join(filePath, x))
   let data = folder.map(x =>  JSON.parse(fs.readFileSync(x, 'utf-8')))
+
   let x = new Promise(function(resolve, reject) {
     resolve(data)
   });
+
   callback(null, {
       statusCode: 200,
       body: JSON.stringify({
