b3e63c1bb6d18d4b63f704197b577086ba71be41
bryan-atencia <bryan.atencia@thetiebar.com>
2020-06-23 23:18:52 -0500

    trying

--- dist/admin/functions/fetchHowTos.js
+++ dist/admin/functions/fetchHowTos.js
@@ -1,20 +1,20 @@
-const fs = require('fs');
-const { join } = require('path');
-const path = require("path")
-
-exports.handler = function(event, context, callback ) {
-  const filePath = '/dist/admin/collections/How-tos';
-  let folder = fs.readdirSync(filePath).map(x => join(filePath, x))
-  let data = folder.map(x =>  JSON.parse(fs.readFileSync(x, 'utf-8')))
-
-  let x = new Promise(function(resolve, reject) {
-    resolve(data)
-  });
-
-  callback(null, {
-      statusCode: 200,
-      body: JSON.stringify({
-        data: data
-      })
-    });
-}
+// const fs = require('fs');
+// const { join } = require('path');
+// const path = require("path")
+//
+// exports.handler = function(event, context, callback ) {
+//   const filePath = process.cwd() + '/dist/admin/collections/How-tos';
+//   let folder = fs.readdirSync(filePath).map(x => join(filePath, x))
+//   let data = folder.map(x =>  JSON.parse(fs.readFileSync(x, 'utf-8')))
+//
+//   let x = new Promise(function(resolve, reject) {
+//     resolve(data)
+//   });
+//
+//   callback(null, {
+//       statusCode: 200,
+//       body: JSON.stringify({
+//         data: data
+//       })
+//     });
+// }
