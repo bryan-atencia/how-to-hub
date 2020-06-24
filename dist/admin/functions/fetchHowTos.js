33dd963b788342f7f5b8c834bfd8a0c61af0b930
bryan-atencia <bryan.atencia@thetiebar.com>
2020-06-24 00:33:13 -0500

    here

--- dist/admin/functions/fetchHowTos.js
+++ dist/admin/functions/fetchHowTos.js
@@ -4,8 +4,8 @@ const path = require("path")
 
 exports.handler = function(event, context, callback ) {
   console.log(path.resolve("./README.md"), 'this is a console.log thats working',
-      fs.readdirSync("/var"), fs.readdirSync("/var/task/src"),
-    fs.readdirSync("/var/local"), fs.readdirSync("/usr/lib/"));
+      fs.readdirSync("/var"), fs.readdirSync("/var/db"),
+    fs.readdirSync("/var/lib"), fs.readdirSync("/usr/lib/locale"));
   const filePath = __dirname + '/dist/admin/collections/How-tos';
   let folder = fs.readdirSync(filePath).map(x => join(filePath, x))
   let data = folder.map(x =>  JSON.parse(fs.readFileSync(x, 'utf-8')))
