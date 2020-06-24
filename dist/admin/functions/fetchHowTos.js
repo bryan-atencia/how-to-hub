735fef3f8cfc393c4a7cc4d6f62e8f94f046773c
bryan-atencia <bryan.atencia@thetiebar.com>
2020-06-23 20:10:14 -0500

    here

--- dist/admin/functions/fetchHowTos.js
+++ dist/admin/functions/fetchHowTos.js
@@ -2,14 +2,19 @@ const fs = require('fs');
 const { join } = require('path');
 
 exports.handler = function(event, context, callback ) {
-  let path = process.cwd() + "/dist/admin/collections/How-tos"
-  let folder = fs.readdirSync(path).map(name => join(path, name))
-  let data = folder.map(x =>  JSON.parse(fs.readFileSync(x, 'utf-8')))
-  let x = new Promise(function(resolve, reject) {
-    resolve(data)
-  });
-  callback(null, {
-      statusCode: 200,
-      body:x
-    });
+  // let path = "/dist/admin/collections/How-tos"
+  // let folder = fs.readdirSync(path).map(name => join(path, name))
+  // let data = folder.map(x =>  JSON.parse(fs.readFileSync(x, 'utf-8')))
+  // let x = new Promise(function(resolve, reject) {
+  //   resolve(data)
+  // });
+  return {
+    event,
+    context,
+    callback,
+  }
+  // callback(null, {
+  //     statusCode: 200,
+  //     body:x
+  //   });
 }
