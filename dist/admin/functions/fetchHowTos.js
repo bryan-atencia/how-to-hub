4851887882c1662f9e8e7e80ac44e671afccd3a7
bryan-atencia <bryan.atencia@thetiebar.com>
2020-06-23 20:15:18 -0500

    here

--- dist/admin/functions/fetchHowTos.js
+++ dist/admin/functions/fetchHowTos.js
@@ -10,10 +10,7 @@ exports.handler = function(event, context, callback ) {
   // });
   callback(null, {
       statusCode: 200,
-      body:{
-        event,
-        context,
-        callback,
-      }
+      event: event,
+      context: context
     });
 }
