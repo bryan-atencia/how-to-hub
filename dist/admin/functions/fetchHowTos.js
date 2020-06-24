e0f1854fe1a3ce41c51d30de2abd7143b2dd240c
bryan-atencia <bryan.atencia@thetiebar.com>
2020-06-23 19:36:54 -0500

    how to function

--- dist/admin/functions/fetchHowTos.js
+++ dist/admin/functions/fetchHowTos.js
@@ -1,12 +1,14 @@
 let fs = require('fs');
 
 exports.handler = function(event, context, callback ) {
-  let path = "./collections/How-tos"
+  let path = "../collections/How-tos"
   let folder = fs.readdirSync(path).map(name => join(path, name))
   let data = folder.map(x =>  JSON.parse(fs.readFileSync(x, 'utf-8')))
-
+  let x = new Promise(function(resolve, reject) {
+    resolve(data)
+  });
   callback(null, {
       statusCode: 200,
-      body: JSON.stringify({ data: data })
+      body:x
     });
 }
